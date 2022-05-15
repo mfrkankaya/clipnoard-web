fe83e90a847c1b43b80b46197de3b54169b3064a
Furkan Kaya <furkan@meditopia.com>
2022-05-16 01:17:56 +0300

    .

--- src/containers/AuthRequired/AuthRequired.tsx
+++ src/containers/AuthRequired/AuthRequired.tsx
@@ -2,30 +2,35 @@ import { FC } from 'react'
 import { useAppSelector } from 'hooks'
 import { Box, Button, Container, Typography } from '@mui/material'
 import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 
 const AuthRequired: FC<{ children: any }> = ({ children }) => {
+  const router = useRouter()
   const { user, isUserInitialized } = useAppSelector((state) => state.app)
 
   if (user) return children
 
-  if (isUserInitialized)
-    return (
-      <Box pt={10}>
-        <Container maxWidth="xs">
-          <Box textAlign="center">
-            <Typography variant="h5">Authentication required.</Typography>
-            <Typography variant="body1" color="text.secondary" my={2}>
-              You're not logged in. You need to login to see this content.
-            </Typography>
+  if (isUserInitialized) {
+    router.push('/auth/welcome')
+    // return (
+    //   <Box pt={10}>
+    //     <Container maxWidth="xs">
+    //       <Box textAlign="center">
+    //         <Typography variant="h5">Authentication required.</Typography>
+    //         <Typography variant="body1" color="text.secondary" my={2}>
+    //           You're not logged in. You need to login to see this content.
+    //         </Typography>
 
-            <NextLink href="/auth/login">
-              <Button size="large" variant='contained'>Login now</Button>
-            </NextLink>
-          </Box>
-        </Container>
-      </Box>
-    )
-
+    //         <NextLink href="/auth/login">
+    //           <Button size="large" variant="contained">
+    //             Login now
+    //           </Button>
+    //         </NextLink>
+    //       </Box>
+    //     </Container>
+    //   </Box>
+    // )
+  }
   return null
 }
 
