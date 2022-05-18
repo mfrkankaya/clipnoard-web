92f8d92f81023b15f28ee2674ca7a4b8da1431c4
Furkan Kaya <furkan@meditopia.com>
2022-05-18 22:25:58 +0300

    add: loading and error state of notes

--- src/containers/NoteList/NoteList.tsx
+++ src/containers/NoteList/NoteList.tsx
@@ -5,12 +5,17 @@ import NoteItem from './NoteItem'
 import { useDebounce, useEffectOnce } from 'usehooks-ts'
 import { useMemo } from 'react'
 import { searchNotes } from 'utils'
+import { Box, Button, CircularProgress, Typography } from '@mui/material'
+import { Add } from '@mui/icons-material'
+import { openCreateModal } from 'store/appSlice'
 
 const NoteList = () => {
   const dispatch = useAppDispatch()
   const user = useAppSelector((state) => state.app.user)
   const searchText = useAppSelector((state) => state.searchBar.searchText)
-  const { isInitialized, data } = useAppSelector((state) => state.notes)
+  const { isInitialized, data, loading } = useAppSelector(
+    (state) => state.notes
+  )
   const debouncedSearchText = useDebounce(searchText, 200)
   const filteredData = useMemo(() => {
     if (!debouncedSearchText) return data
@@ -21,7 +26,28 @@ const NoteList = () => {
     if (!isInitialized) dispatch(fetchInitialNotes(user?.uid as string))
   })
 
-  if (!Boolean(filteredData.length)) return null
+  if (loading)
+    return (
+      <Box mt={10} mx="auto" width="fit-content">
+        <CircularProgress size={48} color="primary" />
+      </Box>
+    )
+
+  if (!Boolean(filteredData.length))
+    return (
+      <Box mt={10} textAlign="center">
+        <Typography variant="h6" color="text.secondary" mb={4}>
+          Looks like you don't have any notes yet.
+        </Typography>
+        <Button
+          variant="outlined"
+          startIcon={<Add />}
+          onClick={() => dispatch(openCreateModal())}
+        >
+          Create one now
+        </Button>
+      </Box>
+    )
 
   return (
     <Masonry
